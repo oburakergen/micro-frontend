2b4635dbddc7912e882e8ab750068c0cb4030663
Burak ERGEN <burak.ergen@tigahealth.com>
2025-04-02 14:09:13 +0300

    add component loader and event bus for micro-frontend architecture

--- workspaces/shell-app/src/index.js
+++ workspaces/shell-app/src/index.js
@@ -1,4 +1,23 @@
-import { loadComponent } from './component-loader';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { setupRouteSynchronization } from './route-sync';
+import { eventBus } from 'event-bus';
 
-document.addEventListener('DOMContentLoaded', async () => {
-});
+setupRouteSynchronization();
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+window.shellApp = {
+  eventBus,
+  navigate: (path) => {
+    window.history.pushState(null, '', path);
+    eventBus.emit('ROUTE_CHANGED', { path });
+  }
+};
